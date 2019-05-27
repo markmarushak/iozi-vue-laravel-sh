a9b7bde4261ebc1e24b5cdb434e29ffea49a008b
Your Name <you@example.com>
2019-05-27 15:11:51 +0300

    pull

--- resources/js/includes/ziggy.js
+++ resources/js/includes/ziggy.js
@@ -1,8 +1,8 @@
     var Ziggy = {
         namedRoutes: {"login":{"uri":"api\/auth\/login","methods":["POST"],"domain":null},"products.index":{"uri":"api\/auth\/products","methods":["GET","HEAD"],"domain":null},"attribute.index":{"uri":"api\/auth\/attribute\/{type}","methods":["GET","HEAD"],"domain":null},"attribute.show":{"uri":"api\/auth\/attribute{id}","methods":["GET","HEAD"],"domain":null},"get.user":{"uri":"api\/auth\/get-user","methods":["GET","HEAD"],"domain":null},"products.store":{"uri":"api\/auth\/products","methods":["POST"],"domain":null},"products.image":{"uri":"api\/auth\/products\/image","methods":["POST"],"domain":null},"products.destroy":{"uri":"api\/auth\/products\/{id}","methods":["DELETE"],"domain":null},"products.update":{"uri":"api\/auth\/products","methods":["PUT"],"domain":null},"attribute.store":{"uri":"api\/auth\/product-attribute","methods":["POST"],"domain":null},"attribute.destroy":{"uri":"api\/auth\/product-attribute\/{id}","methods":["DELETE"],"domain":null},"attribute.update":{"uri":"api\/auth\/product-attribute","methods":["PUT"],"domain":null},"payment.show":{"uri":"api\/auth\/payment","methods":["GET","HEAD"],"domain":null}},
-        baseUrl: 'http://iozi.loc/',
+        baseUrl: 'http://v124285.hosted-by-vdsina.ru/',
         baseProtocol: 'http',
-        baseDomain: 'iozi.loc',
+        baseDomain: 'v124285.hosted-by-vdsina.ru',
         basePort: false,
         defaultParameters: []
     };
